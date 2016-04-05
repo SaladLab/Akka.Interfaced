d9b7cd1458a34f7fb62a9bd13def62f42864761b
Esun Kim <veblush@gmail.com>
2016-04-05 13:20:15 +0900

    Use FAKE.BuildLib

--- build.fsx
+++ build.fsx
@@ -1,279 +1,115 @@
 ﻿#I @"packages/FAKE/tools"
+#I @"packages/FAKE.BuildLib/lib/net451"
 #r "FakeLib.dll"
+#r "BuildLib.dll"
 
 open Fake
-open System.IO
-open Fake.Testing.XUnit2
-open Fake.AssemblyInfoFile
-open Fake.ReleaseNotesHelper
+open BuildLib
 
-// ------------------------------------------------------------------------------ Project
+let solution = 
+    initSolution
+        "./Akka.Interfaced.sln" "Release"
+        [ { // Core Libraries
+            emptyProject with Name = "Akka.Interfaced"
+                              Folder = "./core/Akka.Interfaced"
+                              Dependencies = 
+                                  [ ("Akka.Interfaced-Base", "")
+                                    ("Akka", "") ] }
+          { emptyProject with Name = "Akka.Interfaced-Base"
+                              Folder = "./core/Akka.Interfaced-Base" }
+          { emptyProject with Name = "Akka.Interfaced-SlimClient"
+                              Folder = "./core/Akka.Interfaced-SlimClient"
+                              Dependencies = [ ("Akka.Interfaced-Base", "") ] }
+          { // CodeGenerator-Templates
+            emptyProject with Name = "Akka.Interfaced.Templates"
+                              Folder = "./core/CodeGenerator-Templates"
+                              Template = true
+                              Dependencies = [ ("Akka.Interfaced", "") ] }
+          { emptyProject with Name = "Akka.Interfaced.Templates-Protobuf"
+                              Folder = "./core/CodeGenerator-Templates"
+                              Template = true
+                              Dependencies = 
+                                  [ ("Akka.Interfaced", "")
+                                    ("protobuf-net", "2.0.0.668")
+                                    ("TypeAlias", "1.1.1") ] }
+          { emptyProject with Name = "Akka.Interfaced-SlimClient.Templates"
+                              Folder = "./core/CodeGenerator-Templates"
+                              Template = true
+                              Dependencies = [ ("Akka.Interfaced-SlimClient", "") ] }
+          { emptyProject with Name = "Akka.Interfaced-SlimClient.Templates-Protobuf"
+                              Folder = "./core/CodeGenerator-Templates"
+                              Template = true
+                              Dependencies = 
+                                  [ ("Akka.Interfaced-SlimClient", "")
+                                    ("protobuf-net", "2.0.0.668")
+                                    ("TypeAlias", "1.1.1") ] }
+          { // Plugins
+            emptyProject with Name = "Akka.Interfaced.LogFilter"
+                              Folder = "./plugins/Akka.Interfaced.LogFilter"
+                              Dependencies = 
+                                  [ ("Akka.Interfaced", "")
+                                    ("Akka", "")
+                                    ("Newtonsoft.Json", "") ] }
+          { emptyProject with Name = "Akka.Interfaced.Persistence"
+                              Folder = "./plugins/Akka.Interfaced.Persistence"
+                              Dependencies = 
+                                  [ ("Akka.Interfaced", "")
+                                    ("Akka.Persistence", "") ] }
+          { emptyProject with Name = "Akka.Interfaced.ProtobufSerializer"
+                              Folder = "./plugins/Akka.Interfaced.ProtobufSerializer"
+                              Dependencies = 
+                                  [ ("Akka.Interfaced", "")
+                                    ("protobuf-net", "")
+                                    ("TypeAlias", "") ] }
+          { emptyProject with Name = "Akka.Interfaced.TestKit"
+                              Folder = "./plugins/Akka.Interfaced.TestKit"
+                              Dependencies = [ ("Akka.Interfaced", "") ] } ]
 
-let buildSolutionFile = "./Akka.Interfaced.sln"
-let buildConfiguration = "Release"
+Target "Clean" <| fun _ -> cleanBin
 
-type Project = { 
-    Name: string;
-    Folder: string;
-    Template: bool;
-    AssemblyVersion: string;
-    PackageVersion: string;
-    Releases: ReleaseNotes list;
-    Dependencies: (string * string) list;
-}
+Target "AssemblyInfo" <| fun _ -> generateAssemblyInfo solution
 
-let emptyProject = { Name=""; Folder=""; Template=false; AssemblyVersion="";
-                     PackageVersion=""; Releases=[]; Dependencies=[] }
+Target "Restore" <| fun _ -> restoreNugetPackages solution
 
-let decoratePrerelease v =
-    let couldParse, parsedInt = System.Int32.TryParse(v)
-    if couldParse then "build" + (sprintf "%04d" parsedInt) else v
+Target "Build" <| fun _ -> buildSolution solution
 
-let decoratePackageVersion v =
-    if hasBuildParam "nugetprerelease" then
-        v + "-" + decoratePrerelease((getBuildParam "nugetprerelease"))
-    else
-        v
+Target "Test" <| fun _ -> testSolution solution
 
-let projects = ([
-    // Core Libraries
-    {   emptyProject with
-        Name="Akka.Interfaced";
-        Folder="./core/Akka.Interfaced";
-        Dependencies=[("Akka.Interfaced-Base", "");
-                      ("Akka", "")];
-    };
-    {   emptyProject with
-        Name="Akka.Interfaced-Base";
-        Folder="./core/Akka.Interfaced-Base";
-    };
-    {   emptyProject with
-        Name="Akka.Interfaced-SlimClient";
-        Folder="./core/Akka.Interfaced-SlimClient";
-        Dependencies=[("Akka.Interfaced-Base", "")];
-    };
-    // CodeGenerator-Templates
-    {   emptyProject with
-        Name="Akka.Interfaced.Templates";
-        Folder="./core/CodeGenerator-Templates";
-        Template=true;
-        Dependencies=[("Akka.Interfaced", "")];
-    };
-    {   emptyProject with
-        Name="Akka.Interfaced.Templates-Protobuf";
-        Folder="./core/CodeGenerator-Templates";
-        Template=true;
-        Dependencies=[("Akka.Interfaced", "");
-                      ("protobuf-net", "2.0.0.668");
-                      ("TypeAlias", "1.1.1");];
-    };
-    {   emptyProject with
-        Name="Akka.Interfaced-SlimClient.Templates";
-        Folder="./core/CodeGenerator-Templates";
-        Template=true;
-        Dependencies=[("Akka.Interfaced-SlimClient", "")];
-    };
-    {   emptyProject with
-        Name="Akka.Interfaced-SlimClient.Templates-Protobuf";
-        Folder="./core/CodeGenerator-Templates";
-        Template=true;
-        Dependencies=[("Akka.Interfaced-SlimClient", "");
-                      ("protobuf-net", "2.0.0.668");
-                      ("TypeAlias", "1.1.1");];
-    };
-    // Plugins
-    {   emptyProject with
-        Name="Akka.Interfaced.LogFilter";
-        Folder="./plugins/Akka.Interfaced.LogFilter";
-        Dependencies=[("Akka.Interfaced", "");
-                      ("Akka", "");
-                      ("Newtonsoft.Json", "");];
-    };
-    {   emptyProject with
-        Name="Akka.Interfaced.Persistence";
-        Folder="./plugins/Akka.Interfaced.Persistence";
-        Dependencies=[("Akka.Interfaced", "");
-                      ("Akka.Persistence", "")];
-    };
-    {   emptyProject with
-        Name="Akka.Interfaced.ProtobufSerializer";
-        Folder="./plugins/Akka.Interfaced.ProtobufSerializer";
-        Dependencies=[("Akka.Interfaced", "");
-                      ("protobuf-net", "");
-                      ("TypeAlias", "");];
-    };
-    {   emptyProject with
-        Name="Akka.Interfaced.TestKit";
-        Folder="./plugins/Akka.Interfaced.TestKit";
-        Dependencies=[("Akka.Interfaced", "");];
-    };]
-    |> List.map (fun p -> 
-        let parsedReleases =
-            File.ReadLines (p.Folder @@ (p.Name + ".Release.md"))
-            |> ReleaseNotesHelper.parseAllReleaseNotes
-        let latest = List.head parsedReleases
-        { p with AssemblyVersion = latest.AssemblyVersion;
-                 PackageVersion = decoratePackageVersion(latest.AssemblyVersion);
-                 Releases = parsedReleases }
-    ))
+Target "Cover" <| fun _ ->
+     coverSolutionWithParams 
+        (fun p -> { p with Filter = "+[Akka.Interfaced*]* -[*.Tests]*" })
+        solution
 
-let project name =
-    List.filter (fun p -> p.Name = name) projects |> List.head
-
-let dependencies p deps =
-    p.Dependencies |>
-    List.map (fun d -> match d with 
-                       | (id, "") -> (id, match List.tryFind (fun (x, ver) -> x = id) deps with
-                                          | Some (_, ver) -> ver
-                                          | None -> ((project id).PackageVersion))
-                       | (id, ver) -> (id, ver))
-    
-// ---------------------------------------------------------------------------- Variables
-
-let binDir = "bin"
-let testDir = binDir @@ "test"
-let nugetDir = binDir @@ "nuget"
-let nugetWorkDir = nugetDir @@ "work"
-
-// ------------------------------------------------------------------------------ Targets
-
-Target "Clean" (fun _ -> 
-    CleanDirs [binDir]
-)
-
-Target "AssemblyInfo" (fun _ ->
-    projects
-    |> List.filter (fun p -> not p.Template)
-    |> List.iter (fun p -> 
-        CreateCSharpAssemblyInfo (p.Folder @@ "Properties" @@ "AssemblyInfoGenerated.cs")
-          [ Attribute.Version p.AssemblyVersion
-            Attribute.FileVersion p.AssemblyVersion
-            Attribute.InformationalVersion p.PackageVersion ]
-        )
-)
-
-Target "Build" (fun _ ->
-    !! buildSolutionFile
-    |> MSBuild "" "Rebuild" [ "Configuration", buildConfiguration ]
-    |> Log "Build-Output: "
-)
-
-Target "Test" (fun _ ->  
-    let xunitToolPath = findToolInSubPath "xunit.console.exe" "./packages/FAKE/xunit.runner.console*/tools"
-    ensureDirectory testDir
-    !! ("./core/**/bin/" + buildConfiguration + "/*.Tests.dll")
-    |> xUnit2 (fun p -> 
-        {p with 
-            ToolPath = xunitToolPath;
-            ShadowCopy = false;
-            XmlOutputPath = Some (testDir @@ "TestResult.xml") })
-)
-
-let createNugetPackages _ =
-    projects
-    |> List.iter (fun project -> 
-        let nugetFile = project.Folder @@ project.Name + ".nuspec";
-        let workDir = nugetWorkDir @@ project.Name;
-
-        let dllFileName = project.Folder @@ "bin/Release" @@ project.Name;
-        let dllFiles = (!! (dllFileName + ".dll")
-                        ++ (dllFileName + ".pdb")
-                        ++ (dllFileName + ".xml"))
-        dllFiles |> CopyFiles (workDir @@ "lib" @@ "net45")
-
-        let dllFileNameNet35 = (project.Folder + ".Net35") @@ "bin/Release" @@ project.Name;
-        let dllFilesNet35 = (!! (dllFileNameNet35 + ".dll")
-                             ++ (dllFileNameNet35 + ".pdb")
-                             ++ (dllFileNameNet35 + ".xml"))
-        if (Seq.length dllFilesNet35 > 0) then (
-            dllFilesNet35 |> CopyFiles (workDir @@ "lib" @@ "net35"))
-         
-        let isAssemblyInfo f = (filename f).Contains("AssemblyInfo")
-        let isSrc f = (hasExt ".cs" f) && not (isAssemblyInfo f)
-        CopyDir (workDir @@ "src") project.Folder isSrc
-
-        let packageFile = project.Folder @@ "packages.config"
-        let packageDependencies = if (fileExists packageFile) then (getDependencies packageFile) else []
-
-        NuGet (fun p -> 
-            {p with
-                Project = project.Name
-                OutputPath = nugetDir
-                WorkingDir = workDir
-                Dependencies = dependencies project packageDependencies
-                SymbolPackage = (if (project.Name.Contains("Templates")) then NugetSymbolPackage.None else NugetSymbolPackage.Nuspec)
-                Version = project.PackageVersion 
-                ReleaseNotes = (List.head project.Releases).Notes |> String.concat "\n"
-            }) nugetFile
-    )
-
-let publishNugetPackages _ =
-    projects
-    |> List.iter (fun project -> 
-        try
-            NuGetPublish (fun p -> 
-                {p with
-                    Project = project.Name
-                    OutputPath = nugetDir
-                    WorkingDir = nugetDir
-                    AccessKey = getBuildParamOrDefault "nugetkey" ""
-                    PublishUrl = getBuildParamOrDefault "nugetpublishurl" ""
-                    Version = project.PackageVersion })
-        with e -> if getBuildParam "forcepublish" = "" then raise e; ()
-        if not project.Template && hasBuildParam "nugetpublishurl" then (
-            // current FAKE doesn't support publishing symbol package with NuGetPublish.
-            // To workaround thid limitation, let's tweak Version to cheat nuget read symbol package
-            try
-                NuGetPublish (fun p -> 
-                    {p with
-                        Project = project.Name
-                        OutputPath = nugetDir
-                        WorkingDir = nugetDir
-                        AccessKey = getBuildParamOrDefault "nugetkey" ""
-                        PublishUrl = getBuildParamOrDefault "nugetpublishurl" ""
-                        Version = project.PackageVersion + ".symbols" })
-            with e -> if getBuildParam "forcepublish" = "" then raise e; ()
-        )
-    )
+Target "Coverity" <| fun _ -> coveritySolution solution "SaladLab/Akka.Interfaced"
 
 Target "Nuget" <| fun _ ->
-    createNugetPackages()
-    publishNugetPackages()
+    createNugetPackages solution
+    publishNugetPackages solution
 
 Target "CreateNuget" <| fun _ ->
-    createNugetPackages()
+    createNugetPackages solution
 
 Target "PublishNuget" <| fun _ ->
-    publishNugetPackages()
+    publishNugetPackages solution
 
-Target "Help" (fun _ ->  
-    List.iter printfn [
-      "usage:"
-      "build [target]"
-      ""
-      " Targets for building:"
-      " * Build        Build"
-      " * Test         Build and Test"
-      " * Nuget        Create and publish nugets packages"
-      " * CreateNuget  Create nuget packages"
-      "                [nugetprerelease={VERSION_PRERELEASE}] "
-      " * PublishNuget Publish nugets packages"
-      "                [nugetkey={API_KEY}] [nugetpublishurl={PUBLISH_URL}] [forcepublish=1]"
-      ""]
-)
+Target "CI" <| fun _ -> ()
 
-// --------------------------------------------------------------------------- Dependency
+Target "Help" <| fun _ -> 
+    showUsage solution (fun _ -> None)
 
-// Build order
 "Clean"
   ==> "AssemblyInfo"
+  ==> "Restore"
   ==> "Build"
   ==> "Test"
 
-"Build"
-  ==> "Nuget"
+"Build" ==> "Nuget"
+"Build" ==> "CreateNuget"
+"Build" ==> "Cover"
+"Restore" ==> "Coverity"
 
-"Build"
-  ==> "CreateNuget"
-  
+"Test" ==> "CI"
+"Cover" ==> "CI"
+"Nuget" ==> "CI"
+
 RunTargetOrDefault "Help"
