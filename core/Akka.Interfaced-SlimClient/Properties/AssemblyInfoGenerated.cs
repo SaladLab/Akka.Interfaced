71a69af263e6ec4f88f5635ea1b60b2f12f2412b
Esun Kim <veblush@gmail.com>
2016-04-25 12:39:34 +0900

    Bump version 0.2.1

--- core/Akka.Interfaced-SlimClient/Properties/AssemblyInfoGenerated.cs
+++ core/Akka.Interfaced-SlimClient/Properties/AssemblyInfoGenerated.cs
@@ -2,11 +2,11 @@
 using System.Reflection;
 
 [assembly: AssemblyVersionAttribute("0.0.0")]
-[assembly: AssemblyFileVersionAttribute("0.2.0")]
-[assembly: AssemblyInformationalVersionAttribute("0.2.0")]
+[assembly: AssemblyFileVersionAttribute("0.2.1")]
+[assembly: AssemblyInformationalVersionAttribute("0.2.1")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const string Version = "0.0.0";
-        internal const string InformationalVersion = "0.2.0";
+        internal const string InformationalVersion = "0.2.1";
     }
 }
--- core/CodeGenerator-Templates/Akka.Interfaced.Templates.Release.md
+++ core/CodeGenerator-Templates/Akka.Interfaced.Templates.Release.md
@@ -1,3 +1,7 @@
+## 0.2.1 (Released 2016/04/25)
+
+* Update Akka.NET 1.0.7
+
 ## 0.2.0 (Released 2015/11/09)
 
 ## 0.1.0 (Released 2015/11/08)
