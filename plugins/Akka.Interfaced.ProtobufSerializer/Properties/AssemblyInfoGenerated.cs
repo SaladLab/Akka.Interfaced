0bae2490db28f7de51c41d67a3a2126c30425c0e
Esun Kim <veblush@gmail.com>
2016-05-27 16:36:38 +0900

    Changed version.

--- plugins/Akka.Interfaced.ProtobufSerializer/Properties/AssemblyInfoGenerated.cs
+++ plugins/Akka.Interfaced.ProtobufSerializer/Properties/AssemblyInfoGenerated.cs
@@ -3,10 +3,10 @@ using System.Reflection;
 
 [assembly: AssemblyVersionAttribute("0.0.0")]
 [assembly: AssemblyFileVersionAttribute("0.3.0")]
-[assembly: AssemblyInformationalVersionAttribute("0.3.0")]
+[assembly: AssemblyInformationalVersionAttribute("0.3.0-beta")]
 namespace System {
     internal static class AssemblyVersionInformation {
         internal const string Version = "0.0.0";
-        internal const string InformationalVersion = "0.3.0";
+        internal const string InformationalVersion = "0.3.0-beta";
     }
 }
