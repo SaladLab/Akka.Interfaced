776f7de9259544e79f00a438dbcf101fefcd1b74
Esun Kim <veblush@gmail.com>
2016-05-27 13:35:59 +0900

    Strip useless using.

--- samples/Protobuf/Interface/ISurrogate.cs
+++ samples/Protobuf/Interface/ISurrogate.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Akka.Actor;
 using Akka.Interfaced;
-using ProtoBuf;
 
 namespace Protobuf.Interface
 {
