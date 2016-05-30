8a9d33d0eb502af98432d715607583840d0c64a7
Esun Kim <veblush@gmail.com>
2016-05-30 12:07:20 +0900

    Make ResponsiveExceptionFilter.Order configurable.

--- core/Akka.Interfaced/ResponsiveExceptionFilter.cs
+++ core/Akka.Interfaced/ResponsiveExceptionFilter.cs
@@ -6,15 +6,28 @@ namespace Akka.Interfaced
     [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
     public class ResponsiveExceptionAttribute : Attribute, IFilterPerClassFactory
     {
+        public const int FilterDefaultOrder = 10;
+
+        private int _filterOrder;
         private Func<Exception, bool> _filter;
 
-        public ResponsiveExceptionAttribute(Func<Exception, bool> filter)
+        public ResponsiveExceptionAttribute(int filterOrder = FilterDefaultOrder, Func<Exception, bool> filter = null)
         {
+            if (filter == null)
+                throw new ArgumentNullException(nameof(filter));
+
+            _filterOrder = filterOrder;
             _filter = filter;
         }
 
         public ResponsiveExceptionAttribute(params Type[] exceptionTypes)
+            : this(FilterDefaultOrder, exceptionTypes)
         {
+        }
+
+        public ResponsiveExceptionAttribute(int filterOrder, params Type[] exceptionTypes)
+        {
+            _filterOrder = filterOrder;
             _filter = e => exceptionTypes.Any(t => e.GetType() == t);
         }
 
@@ -24,18 +37,20 @@ namespace Akka.Interfaced
 
         IFilter IFilterPerClassFactory.CreateInstance()
         {
-            return new ResponsiveExceptionFilter(_filter);
+            return new ResponsiveExceptionFilter(_filterOrder, _filter);
         }
     }
 
     public class ResponsiveExceptionFilter : IPostRequestFilter
     {
+        private int _filterOrder;
         private Func<Exception, bool> _filter;
 
-        int IFilter.Order => 0;
+        int IFilter.Order => _filterOrder;
 
-        public ResponsiveExceptionFilter(Func<Exception, bool> filter)
+        public ResponsiveExceptionFilter(int filterOrder, Func<Exception, bool> filter)
         {
+            _filterOrder = filterOrder;
             _filter = filter;
         }
 
