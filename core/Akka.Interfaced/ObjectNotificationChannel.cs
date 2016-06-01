ef3d336ac5c7d5334f3b3bd119802695f8bd56b7
Esun Kim <veblush@gmail.com>
2016-06-01 15:20:23 +0900

    Add ObjectNotificationChannel.

---
+++ core/Akka.Interfaced/ObjectNotificationChannel.cs
@@ -0,0 +1,39 @@
+﻿namespace Akka.Interfaced
+{
+    public class ObjectNotificationChannel : INotificationChannel
+    {
+        public IInterfacedObserver Observer { get; }
+
+        public ObjectNotificationChannel(IInterfacedObserver observer)
+        {
+            Observer = observer;
+        }
+
+        public void Notify(NotificationMessage notificationMessage)
+        {
+            notificationMessage.InvokePayload.Invoke(Observer);
+        }
+
+        public override bool Equals(object obj)
+        {
+            var c = obj as ObjectNotificationChannel;
+            if (c == null)
+                return false;
+
+            return Observer.Equals(c.Observer);
+        }
+
+        public override int GetHashCode()
+        {
+            return Observer.GetHashCode();
+        }
+
+        public static TObserver Create<TObserver>(TObserver observer)
+            where TObserver : IInterfacedObserver
+        {
+            var proxy = InterfacedObserver.Create(typeof(TObserver));
+            proxy.Channel = new ObjectNotificationChannel(observer);
+            return (TObserver)(object)proxy;
+        }
+    }
+}
