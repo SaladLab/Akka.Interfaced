83b8345012169220367a530c2ac24cea32270aa7
Esun Kim <veblush@gmail.com>
2016-06-08 14:47:55 +0900

    Add filter demo to samples/Manual.

--- samples/Manual/Program/DemoFilter.cs
+++ samples/Manual/Program/DemoFilter.cs
@@ -14,5 +14,112 @@ namespace Manual
         {
             _system = system;
         }
+
+        [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
+        public sealed class LogAttribute : Attribute, IFilterPerClassFactory, IPreRequestFilter, IPostRequestFilter
+        {
+            void IFilterPerClassFactory.Setup(Type actorType) { }
+
+            IFilter IFilterPerClassFactory.CreateInstance() => this;
+
+            int IFilter.Order => 1;
+
+            void IPreRequestFilter.OnPreRequest(PreRequestFilterContext context)
+            {
+                if (context.Handled)
+                    return;
+
+                Console.Write("Request: " + context.Request.InvokePayload.GetType().Name);
+            }
+
+            void IPostRequestFilter.OnPostRequest(PostRequestFilterContext context)
+            {
+                if (context.Response.Exception != null)
+                    Console.WriteLine(" -> ! " + context.Response.Exception.GetType().Name);
+                else if (context.Response.ReturnPayload != null)
+                    Console.WriteLine(" -> " + context.Response.ReturnPayload.Value);
+                else
+                    Console.WriteLine(" -> Done" + context.Response.ReturnPayload.Value);
+            }
+        }
+
+        [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
+        public sealed class AuthorizedAttribute : Attribute, IFilterPerClassFactory, IPreRequestFilter
+        {
+            void IFilterPerClassFactory.Setup(Type actorType) { }
+
+            IFilter IFilterPerClassFactory.CreateInstance() => this;
+
+            int IFilter.Order => 2;
+
+            void IPreRequestFilter.OnPreRequest(PreRequestFilterContext context)
+            {
+                if (context.Handled)
+                    return;
+
+                var actor = (MyActor)context.Actor;
+                if (actor.Permission < 1)
+                {
+                    context.Response = new ResponseMessage
+                    {
+                        RequestId = context.Request.RequestId,
+                        Exception = new InvalidOperationException("Not enough permission.")
+                    };
+                    return;
+                }
+            }
+        }
+
+        [Log, ResponsiveException(typeof(ArgumentException))]
+        public class MyActor : InterfacedActor, IGreeterSync
+        {
+            private int _count;
+
+            public int Permission { get; }
+
+            public MyActor(int permission)
+            {
+                Permission = permission;
+            }
+
+            string IGreeterSync.Greet(string name)
+            {
+                if (string.IsNullOrEmpty(name))
+                    throw new ArgumentException(nameof(name));
+
+                _count += 1;
+                return $"Hello {name}!";
+            }
+
+            [Authorized]
+            int IGreeterSync.GetCount()
+            {
+                return _count;
+            }
+        }
+
+        private async Task DoWork(int permission)
+        {
+            Console.WriteLine($"- Work with permission={permission}");
+
+            var greeter = new GreeterRef(_system.ActorOf(Props.Create(() => new MyActor(permission))));
+
+            try
+            {
+                await greeter.Greet("Hello");
+                await greeter.Greet("Actor");
+                await greeter.GetCount();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private async Task DemoBasic()
+        {
+            await DoWork(0);
+            await DoWork(1);
+        }
     }
 }
--- samples/Manual/Program/DemoLogFilter.cs
+++ samples/Manual/Program/DemoLogFilter.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Akka.Interfaced;
 using Akka.Actor;
+using Akka.Interfaced.LogFilter;
+using NLog;
 
 namespace Manual
 {
@@ -13,6 +15,50 @@ namespace Manual
         public DemoLogFilter(ActorSystem system, string[] args)
         {
             _system = system;
+        }
+
+        [Log, ResponsiveException(typeof(ArgumentException))]
+        private class GreetingActor : InterfacedActor, IGreeterSync
+        {
+            private static Logger s_logger = LogManager.GetCurrentClassLogger();
+            private int _count;
+
+            string IGreeterSync.Greet(string name)
+            {
+                if (string.IsNullOrEmpty(name))
+                    throw new ArgumentException(nameof(name));
+                _count += 1;
+                return $"Hello {name}!";
+            }
+
+            int IGreeterSync.GetCount()
+            {
+                return _count;
+            }
+
+            [MessageHandler]
+            private void OnMessage(string message)
+            {
+                Console.WriteLine("Message: " + message);
+            }
+        }
+
+        private async Task DemoLogRequest()
+        {
+            var actor = _system.ActorOf<GreetingActor>();
+            var greeter = new GreeterRef(actor);
+            Console.WriteLine(await greeter.Greet("World"));
+            try
+            {
+                Console.WriteLine(await greeter.Greet(null));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.GetType().Name);
+            }
+            Console.WriteLine(await greeter.GetCount());
+            greeter.Actor.Tell("Bye!");
+            await actor.GracefulStop(TimeSpan.FromMinutes(1), InterfacedPoisonPill.Instance);
         }
     }
 }
