953001651c0ffb9863ca68ea6a3d87ccc181da95
Esun Kim <veblush@gmail.com>
2016-06-23 11:13:16 +0900

    Add typed OpenChannel to IActorBoundGateway.

--- plugins/Akka.Interfaced.SlimServer/Interface/IActorBoundGateway.cs
+++ plugins/Akka.Interfaced.SlimServer/Interface/IActorBoundGateway.cs
@@ -5,6 +5,7 @@ namespace Akka.Interfaced.SlimServer
 {
     public interface IActorBoundGateway : IInterfacedActor
     {
+        Task<string> OpenChannel(InterfacedActorRef actor, ActorBindingFlags bindingFlags = 0);
         Task<string> OpenChannel(IActorRef actor, TaggedType[] types, ActorBindingFlags bindingFlags = 0);
     }
 }
